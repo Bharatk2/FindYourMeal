492899ba87eebc9f4fcf7ac1265b54de988853d0
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-16 20:41:38 -0700

    Implemented Model & Core Data Model

--- FreeMeals/FreeMeals/Model/MealRepresentation.swift
+++ FreeMeals/FreeMeals/Model/MealRepresentation.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+class MealRepresentation: Codable {
+    var meals: [Meal]
+    
+    enum CodingKeys: String, CodingKey {
+        case meals
+    }
+    
+    
+    
+    class Meal: Codable {
+        let id: String
+        let mealName: String
+        let category: String
+        let area: String
+        let instruction: String
+        let mealThumb: String
+        
+        
+        
+        enum MealKeys: String, CodingKey {
+            case id = "idMeal"
+            case mealName = "strMeal"
+            case category = "strCategory"
+            case area = "strArea"
+            case instruction = "strInstructions"
+            case mealThumb = "strMealThumb"
+        }
+        
+        
+        
+        required init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: MealRepresentation.CodingKeys.self)
+            var mealContainer = try container.nestedUnkeyedContainer(forKey: .meals)
+            let mealDictionary = try mealContainer.nestedContainer(keyedBy: MealKeys.self)
+            id = try mealDictionary.decode(String.self, forKey: .id)
+            mealName = try mealDictionary.decode(String.self, forKey: .mealName)
+            category = try mealDictionary.decode(String.self, forKey: .category)
+            area = try mealDictionary.decode(String.self, forKey: .area)
+            instruction = try mealDictionary.decode(String.self, forKey: .instruction)
+            mealThumb = try mealDictionary.decode(String.self, forKey: .mealThumb)
+            
+        }
+    }
+}
