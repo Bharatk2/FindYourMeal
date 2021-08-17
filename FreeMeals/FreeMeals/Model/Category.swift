50a09f91a52f3bd0ccdc79c1c077e4f944cece8e
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-16 21:17:14 -0700

    Implemented Category swift model

--- FreeMeals/FreeMeals/Model/Category.swift
+++ FreeMeals/FreeMeals/Model/Category.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class Categories: Codable {
+    var categories: [CategoryRepresentation]
+    
+    enum CodingKeys: String, CodingKey {
+        case categories
+    }
+    
+    class CategoryRepresentation: Codable {
+        var id: String
+        var category: String
+        var categoryThumb: String
+        
+        enum CategoryKeys: String, CodingKey {
+            case id = "idCategory"
+            case category = "strCategory"
+            case categoryThumb = "strCategoryThumb"
+        }
+        
+        required init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: Categories.CodingKeys.self)
+            var categoriesContainer = try container.nestedUnkeyedContainer(forKey: .categories)
+            let categoryDictionary = try categoriesContainer.nestedContainer(keyedBy: CategoryKeys.self)
+            id = try categoryDictionary.decode(String.self, forKey: .id)
+            category = try categoryDictionary.decode(String.self, forKey: .category)
+            categoryThumb = try categoryDictionary.decode(String.self, forKey: .categoryThumb)
+            
+        }
+    }
+}
