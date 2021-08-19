ff4159030c3032abfab74881699afbb1aa8fc203
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-18 21:33:30 -0700

    Added http method, dataloader, cache

--- FreeMeals/FreeMeals/Extensions+Utilities/Endpoints+HTTPMethods.swift
+++ FreeMeals/FreeMeals/Extensions+Utilities/Endpoints+HTTPMethods.swift
@@ -14,3 +14,10 @@ extension Endpoints {
     static let meals = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?c=")!
     static let mealById = URL(string: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=")!
 }
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
