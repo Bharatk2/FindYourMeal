e877b0a3e6f71cc3ee3e62bd94f51b247f32eda1
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-18 21:46:29 -0700

    Added properties to the model controller

--- FreeMeals/FreeMeals/ModelController/ModelController.swift
+++ FreeMeals/FreeMeals/ModelController/ModelController.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+enum NetworkError: Error {
+    case noData(String), badData(String)
+    case failedFetch(String), badURL(String)
+    case badError(String)
+}
+
+class ModelController {
+    
+    // MARK: - Properties
+    var categories: [Categories.CategoryRepresentation] = []
+    var imageCache = Cache<NSString, AnyObject>()
+    var mealCache = Cache<String, Meal>()
+    var bgContext = CoreDataStack.shared.container.newBackgroundContext()
+    var dataLoader: DataLoader?
+    let operationQueue = OperationQueue()
+    static var shared = ModelController()
+    
+    
+    
+}
