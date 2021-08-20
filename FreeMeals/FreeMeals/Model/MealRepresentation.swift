6c4b0a21ddd45cd3157fab90880efddfcd02cde4
Bharat Kumar <bharat.kumar@dexcom.com>
2021-08-20 14:47:40 -0700

    Added collection view controller

--- FreeMeals/FreeMeals/Model/MealRepresentation.swift
+++ FreeMeals/FreeMeals/Model/MealRepresentation.swift
@@ -34,57 +34,59 @@ class MealRepresentation: Codable {
     }
 }
 
-class MealDetailRepresentation: Codable {
-    var meals: [MealDetail]
-    
-    enum CodingKeys: String, CodingKey {
-        case meals
-    }
-    
-    class MealDetail: Codable {
-        let id: String
-        let mealName: String
-        let instructions: String
-        let ingredients: [String]
-        let measure: String
-        let category: String
-        
-        enum IngredientsKeys: String, CodingKey {
-            case id = "idMeal"
-            case mealName = "strMeal"
-            case instructions = "strInstructions"
-            case category = "strCategory"
-            case ingredient1 = "strIngredient1"
-            case ingredient2 = "strIngredient2"
-            case ingredient3 = "strIngredient3"
-            case ingredient4 = "strIngredient4"
-            case ingredient5 = "strIngredient5"
-            case ingredient6 = "strIngredient6"
-            case ingredient7 = "strIngredient7"
-            case ingredient8 = "strIngredient8"
-            case ingredient9 = "strIngredient9"
-            case ingredient10 = "strIngredient10"
-            case measure1 = "strMeasure1"
-            case measure2 = "strMeasure2"
-            case measure3 = "strMeasure3"
-            case measure4 = "strMeasure4"
-            case measure5 = "strMeasure5"
-            case measure6 = "strMeasure6"
-            case measure7 = "strMeasure7"
-            case measure8 = "strMeasure8"
-            case measure9 = "strMeasure9"
-            case measure10 = "strMeasure10"
-        }
-        
-  
-        
-        required init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: IngredientsKeys.self)
-            id = try container.decode(String.self, forKey: .id)
-            mealName = try container.decode(String.self, forKey: .mealName)
-            instructions = try container.decode(String.self, forKey: .instructions)
-            category = try container.decode(String.self, forKey: .category)
-        
-        }
-    }
-}
+//class MealDetailRepresentation: Codable {
+//    var meals: [MealDetail]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case meals
+//    }
+//
+//    class MealDetail: Codable {
+//        let id: String
+//        let mealName: String
+//        let instructions: String
+//        let ingredients: String
+//        let measure: String
+//        let category: String
+//
+//        enum IngredientsKeys: String, CodingKey {
+//            case id = "idMeal"
+//            case mealName = "strMeal"
+//            case instructions = "strInstructions"
+//            case category = "strCategory"
+//            case ingredient1 = "strIngredient1"
+//            case ingredient2 = "strIngredient2"
+//            case ingredient3 = "strIngredient3"
+//            case ingredient4 = "strIngredient4"
+//            case ingredient5 = "strIngredient5"
+//            case ingredient6 = "strIngredient6"
+//            case ingredient7 = "strIngredient7"
+//            case ingredient8 = "strIngredient8"
+//            case ingredient9 = "strIngredient9"
+//            case ingredient10 = "strIngredient10"
+//            case measure1 = "strMeasure1"
+//            case measure2 = "strMeasure2"
+//            case measure3 = "strMeasure3"
+//            case measure4 = "strMeasure4"
+//            case measure5 = "strMeasure5"
+//            case measure6 = "strMeasure6"
+//            case measure7 = "strMeasure7"
+//            case measure8 = "strMeasure8"
+//            case measure9 = "strMeasure9"
+//            case measure10 = "strMeasure10"
+//
+//        }
+//
+//        required init(from decoder: Decoder) throws {
+//            let container = try decoder.container(keyedBy: IngredientsKeys.self)
+//            id = try container.decode(String.self, forKey: .id)
+//            mealName = try container.decode(String.self, forKey: .mealName)
+//            instructions = try container.decode(String.self, forKey: .instructions)
+//            category = try container.decode(String.self, forKey: .category)
+//            ingredients = try container
+//                .decode(String.self, forKey: .ingredient1)
+//
+//
+//        }
+//    }
+//}
