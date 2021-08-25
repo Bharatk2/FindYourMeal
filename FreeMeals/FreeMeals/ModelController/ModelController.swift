cb3868d4185db0abb95e5a647aa00135327e07f8
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-25 11:22:38 -0700

    Added documentation for mealDetails networking call

--- FreeMeals/FreeMeals/ModelController/ModelController.swift
+++ FreeMeals/FreeMeals/ModelController/ModelController.swift
@@ -64,9 +64,9 @@ class ModelController {
             do {
                 let categories = try self.decoder.decode(categories, from: data)
                 
-
+                
                 self.categories = categories.categories
-             
+                
                 return completion(categories, nil)
             } catch {
                 return completion(nil, NetworkError.badData("there was an error decoding data"))
@@ -75,7 +75,7 @@ class ModelController {
     }
     
     func getMeals(category: String,completion: @escaping (MealRepresentation?, Error?) -> Void) {
-     
+        
         let requestURL = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?c=\(category)")!
         var request = URLRequest(url: requestURL)
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
@@ -98,11 +98,11 @@ class ModelController {
             
             do {
                 let meals = try self.decoder.decode(meals, from: data)
-
+                
                 self.meals = meals.meals
                 return completion(meals, nil)
                 
-              
+                
             } catch {
                 return completion(nil, NetworkError.badData("there was an error decodig meal data "))
             }
@@ -111,12 +111,12 @@ class ModelController {
         
     }
     
-    /*
-   There is too many ingredients and measures to handle, so I am making an array that will increase the name by number in the end if there is a value
-     */
-    
+    /// Networking function for meals details like instructions, ingredients, measures in detail view.
+    /// - Parameters:
+    ///   - mealID: The meal id to get full access of meal details
+    ///   - completion: Completion handler takes mealDetail as parameter which determines if fetching call succeeded or not.
     func getMealsById(mealID: String,completion: @escaping (MealDetail?, Error?) -> Void) {
-     
+        
         let requestURL = URL(string: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(mealID)")!
         var request = URLRequest(url: requestURL)
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
@@ -134,7 +134,11 @@ class ModelController {
                 completion(nil, NetworkError.badData("No data was returned for meals"))
                 return
             }
-            
+            /*
+             Using JSONSerialization here instead of Codable because the way the API structured its data isn't set up the best,
+             since there are many properties for ingredients and measures. JSONSerialization was the best way to make an array with if
+             statement to determine an existing value and convert it to an array.
+             */
             if let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
                let meals = json["meals"] as? [[String: Any]],
                let mealName = meals[0]["strMeal"] as? String, meals.count > 0 {
@@ -143,26 +147,28 @@ class ModelController {
                 let category = meals[0]["strCategory"] as? String ?? ""
                 let instructions = meals[0]["strInstructions"] as? String ?? ""
                 let mealThumb = meals[0]["strMealThumb"] as? String ?? ""
-            
-            
-            var ingredients: [MealIngredients] = []
-            
-            for number in 1...20 {
-                let ingredient = meals[0]["strIngredient\(number)"] as? String ?? ""
                 
-                if ingredient != "" {
-                    let measure = meals[0]["strMeasure\(number)"] as? String ?? ""
-                    let mealIngredients = MealIngredients(name: ingredient, quantity: measure)
-                    ingredients.append(mealIngredients)
+                
+                var ingredients: [MealIngredients] = []
+                /* here we are making a for loop of int which will iterate till 20 adding it to "strIngredient".
+                 So we don't have to make multiple ingredient properties with number and same with measures.
+                 */
+                for number in 1...20 {
+                    let ingredient = meals[0]["strIngredient\(number)"] as? String ?? ""
+                    // there could be some ingredient properties that can be empty, so we are checking that with if statement.
+                    if ingredient != "" {
+                        let measure = meals[0]["strMeasure\(number)"] as? String ?? ""
+                        let mealIngredients = MealIngredients(name: ingredient, quantity: measure)
+                        ingredients.append(mealIngredients)
+                    }
                 }
-            }
                 // We have the data we are required we will add it to the mealdetail struct and add it to completion
                 let meal = MealDetail(id: id, mealName: mealName, category: category, instructions: instructions, mealThumb: mealThumb, ingredients: ingredients)
                 
                 DispatchQueue.main.async {
                     completion(meal, nil)
                 }
-            
+                
             }
         })
         
