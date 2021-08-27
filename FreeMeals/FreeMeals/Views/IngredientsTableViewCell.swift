413bf3a8addf633978b5ed566eed9014034b5695
Bharat Kumar <Bharkumar8@gmail.com>
2021-08-27 14:37:13 -0700

    Implemented ingredients TableView Cell

--- FreeMeals/FreeMeals/Views/IngredientsTableViewCell.swift
+++ FreeMeals/FreeMeals/Views/IngredientsTableViewCell.swift
@@ -8,16 +8,48 @@
 import UIKit
 
 class IngredientsTableViewCell: UITableViewCell {
-
+    var ingredientslabel = UILabel()
+    var measureLabel = UILabel()
+    var ingredient: MealIngredients? {
+        didSet {
+            updateViews()
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+       handleConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+   
+    func handleConstraints() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .white
+        
+        ingredientslabel.font = UIFont(name: "AvenirNext-Regular", size: 11)
+        ingredientslabel.textColor = UIColor(named: "#4A4A4A")
+        ingredientslabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(ingredientslabel)
+        
+        // categoryNameLabel label Constraints
+        ingredientslabel.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 5).isActive = true
+        ingredientslabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        ingredientslabel.trailingAnchor.constraint(equalTo: measureLabel.trailingAnchor, constant: -20).isActive = true
+        
+        // mealNameLabel name setup
+        measureLabel.font = UIFont(name: "AvenirNext-Regular", size: 11)
+        measureLabel.textColor = UIColor(named: "#4A4A4A")
+        addSubview(measureLabel)
+        
+        // mealNameLabel name constraints
+        measureLabel.translatesAutoresizingMaskIntoConstraints = false
+        measureLabel.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 5).isActive = true
+        measureLabel.leadingAnchor.constraint(equalTo: ingredientslabel.trailingAnchor, constant: 20).isActive = true
+        measureLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+        
     }
-
+    
+    func updateViews() {
+        guard let ingredient = ingredient else { return }
+        ingredientslabel.text = ingredient.name
+        measureLabel.text = ingredient.quantity
+    }
 }
